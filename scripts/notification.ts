68306fac193f9a905004b0675c5430528678eb1c
Wasupon <wasupon.angsupokai@thomsonreuters.com>
2020-02-12 15:20:47 +0700

    remove console.log

--- scripts/notification.ts
+++ scripts/notification.ts
@@ -6,7 +6,6 @@ module.exports = function () {
     // Let's check whether notification permissions have already been granted
     else if (Notification.permission === "granted") {
         // If it's okay let's create a notification
-        console.log('Notification.permission === granted');
         notification = new Notification('Electron Notification', {
             'body': 'Notification is coming to townnnn',
             'icon': 'http://placekitten.com/300/300'
@@ -14,7 +13,6 @@ module.exports = function () {
     }
     // Otherwise, we need to ask the user for permission
     else if (Notification.permission !== "denied") {
-        console.log('Notification.permission !== denied');
         Notification.requestPermission().then(function (permission) {
             // If the user accepts, let's create a notification
             if (permission === "granted") {
